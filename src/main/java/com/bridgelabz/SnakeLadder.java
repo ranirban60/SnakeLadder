d13f4ad7b0700801c116008dc5be41c80b4e8aeb
ranirban60@gmail.com <ranirban60@gmail.com>
2022-07-26 23:47:09 +0530

    Added file UC4

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -1,27 +1,39 @@
 package com.bridgelabz;
 
 public class SnakeLadder {
+      static int  start = 0;
+      static int end = 100;
+
     public static void main(String[] args) {
-        int position =0;
-        int dieNumber = (int)(Math.random()*10)%6+1;
-        System.out.println("DieNumber:" +dieNumber);
-        int option = (int)(Math.random()*10)%3;
-        switch(option){
-            case 1:
-                System.out.println("No Play");
-                position +=0;
-                break;
-            case 2:
-                System.out.println("Ladder");
-                position += dieNumber;
-                break;
-            case 3:
-                System.out.println("Snake");
-                position -= dieNumber;
-                break;
+        int position = 0;
+        int dieRole=0;
+        while( position != end) {
+            dieRole++;
+            int dieNumber = (int) (Math.random() * 10) % 6 + 1;
+            System.out.println("DieNumber:" +dieNumber);
+            int option = (int) (Math.random() * 10) % 3;
+            switch (option) {
+                case 0:
+                    System.out.println("No Play");
+                    position += 0;
+                    break;
+                case 1:
+                    System.out.println("Ladder");
+                    position += dieNumber;
+                    break;
+                case 2:
+                    System.out.println("Snake");
+                    position -= dieNumber;
+                    break;
+            }
 
+            if (position == 100) {
+                break;
+            } else if (position < 0) {
+                position = start;
+                System.out.println("Go to Start");
+            }
         }
-        System.out.println(option);
-        }
+        System.out.println("You are Winner, Position is : " +position);
     }
-
+}
