b0235a55e6b79a6574bbd856fcab35de67492b86
ranirban60@gmail.com <ranirban60@gmail.com>
2022-07-26 21:41:51 +0530

    Added file UC2

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -3,7 +3,8 @@ package com.bridgelabz;
 public class SnakeLadder {
     public static void main(String[] args) {
         int position =0;
-        System.out.println("Position:" +position);
+        int dieNumber = (int)(Math.random()*10)%6+1;
+        System.out.println("DieNumber:" +dieNumber);
         }
     }
 
