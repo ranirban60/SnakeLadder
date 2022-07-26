8980361095e4ebaa57b1dc7b281b42edc0c38404
ranirban60@gmail.com <ranirban60@gmail.com>
2022-07-26 21:57:04 +0530

    Added file UC3

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -5,6 +5,23 @@ public class SnakeLadder {
         int position =0;
         int dieNumber = (int)(Math.random()*10)%6+1;
         System.out.println("DieNumber:" +dieNumber);
+        int option = (int)(Math.random()*10)%3;
+        switch(option){
+            case 1:
+                System.out.println("No Play");
+                position +=0;
+                break;
+            case 2:
+                System.out.println("Ladder");
+                position += dieNumber;
+                break;
+            case 3:
+                System.out.println("Snake");
+                position -= dieNumber;
+                break;
+
+        }
+        System.out.println(option);
         }
     }
 
